18ef3817be96679b5a100b9f87f68321ccbcc5bd
benanamen <benanamen@users.noreply.github.com>
2024-06-01 21:15:43 -0700

    Cleaned up example code

--- examples/index.php
+++ examples/index.php
@@ -3,27 +3,15 @@
 require_once './vendor/autoload.php';
 
 use PerfectApp\Container\Container;
-use PerfectApp\Controllers\HomeController;
-use PerfectApp\Controllers\UserController;
 use PerfectApp\Logger\FileLogger;
 use PerfectApp\Routing\Router;
-use PerfectApp\Services\UserService;
 
 $logger = new FileLogger('errors.log');
 
-$container = new Container($logger);
-
-// Bindings
-$container->bind(UserService::class, UserService::class);
-$container->bind(UserController::class, UserController::class);
-$container->bind(HomeController::class, HomeController::class);
+$container = new Container();
 
 $router = new Router($container);
 $router->autoRegisterControllers(__DIR__ . '/src/Controllers');
-
-$requestUri = $_SERVER['REQUEST_URI'];
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
 
 // A user-defined exception handler function
 $router->setNotFoundHandler(function ($requestUri, $requestMethod) use ($logger) {
@@ -33,11 +21,9 @@ $router->setNotFoundHandler(function ($requestUri, $requestMethod) use ($logger)
 });
 
 try {
-$router->dispatch($requestUri, $requestMethod);
+    $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 } catch (RuntimeException $e) {
-    // Log the error
     $logger->error($e->getMessage());
-    // Send a generic 404 response if no user-defined handler is set
     http_response_code(404);
     echo "Route not found.";
 }
